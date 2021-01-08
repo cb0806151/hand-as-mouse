b01aaf07d70f1e7181b556bb8680214a70dcd2ba
cb0806151 <cb0806151@otc.edu>
2021-01-08 16:17:01 -0600

    install tensorflow packages and setup webcam feed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
-import { bro } from './bro.js'
+window.addEventListener("DOMContentLoaded", () => {
+    var video = document.querySelector("#videoElement");
 
-console.log(bro('Dude'))
+    if (navigator.mediaDevices.getUserMedia) {
+        navigator.mediaDevices.getUserMedia({ video: {width: {exact: 1920}, height: {exact: 1080}} })
+            .then(function (stream) {
+                video.srcObject = stream;
+            })
+            .catch(function (err0r) {
+                console.log("Something went wrong!");
+            });
+    }
+});
