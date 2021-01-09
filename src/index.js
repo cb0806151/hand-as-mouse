8cfdebdb8f4fe8c8900dd12a9eecad7d1e797eb2
cb0806151 <cb0806151@otc.edu>
2021-01-09 10:45:07 -0600

    implement detection of hand on webcam

--- src/index.js
+++ src/index.js
@@ -1,13 +1,69 @@
-window.addEventListener("DOMContentLoaded", () => {
-    var video = document.querySelector("#videoElement");
+import * as tf from '@tensorflow/tfjs-core';
+import '@tensorflow/tfjs-backend-webgl';
+import * as handpose from "@tensorflow-models/handpose";
 
+var video = document.querySelector("#videoElement");
+var canvas = document.querySelector("#canvasElement");
+var ctx = canvas.getContext("2d");
+var videoConfig = {width: {exact: 1280}, height: {exact: 720}}
+
+const runHandPose = async () => {
+    const net = await handpose.load()
+    console.log('Handpose model loaded')
+    
+    setInterval(() => {
+        detect(net, )
+    }, 25);
+}
+
+const detect = async (net) => {
+    const hand = await net.estimateHands(video);
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawHand(hand);
+
+}
+
+const drawHand = (predictions) => {
+    if (predictions.length>0) {
+        predictions.forEach((prediction) => {
+            const landmarks = prediction.landmarks;
+
+            for (let i = 0; i<landmarks.length; i++) {
+                const x = landmarks[i][0]
+                const y = landmarks[i][1]
+
+                ctx.beginPath();
+                ctx.arc(x, y, 5, 0, 3 * Math.PI)
+
+                ctx.fillStyle = "indigo"
+                ctx.fill();
+            }
+        })
+    }
+}
+
+const setupWidthAndHeight = () => {
+    video.width = videoConfig.width.exact;
+    video.height = videoConfig.height.exact;
+    
+    canvas.width = videoConfig.width.exact;
+    canvas.height = videoConfig.height.exact
+}
+
+const setupCamera = async () => {
     if (navigator.mediaDevices.getUserMedia) {
-        navigator.mediaDevices.getUserMedia({ video: {width: {exact: 1920}, height: {exact: 1080}} })
+        navigator.mediaDevices.getUserMedia({ video: videoConfig })
             .then(function (stream) {
                 video.srcObject = stream;
+                setupWidthAndHeight();
+                runHandPose()
             })
-            .catch(function (err0r) {
+            .catch(function (error) {
                 console.log("Something went wrong!");
             });
     }
+}
+
+window.addEventListener("DOMContentLoaded", () => {
+    setupCamera()
 });
