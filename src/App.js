ef3dadf8028b93e72bc0b51d048f887bd8ffd5d8
cb0806151 <cb0806151@otc.edu>
2021-01-11 10:28:57 -0600

    integrate hand pose using basic elements

--- src/App.js
+++ src/App.js
@@ -1,40 +1,41 @@
 import '@tensorflow/tfjs-backend-webgl';
 import * as handpose from "@tensorflow-models/handpose";
 import './App.css';
-import React, { useRef, Component } from "react";
-import Webcam from "react-webcam";
+import React, { Component } from "react";
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      webcamRef: useRef(null),
-      canvasRef: useRef(null),
+      video: null,
+      canvas: null,
+      ctx: null,
       videoConfig: {width: 1280, height: 720},
      };
   }
 
-  componentDidUpdate = () => {
-    this.runHandPose();
+  componentDidMount = () => {
+    this.setState({
+      video: document.querySelector("#videoElement"),
+      canvas: document.querySelector("#canvasElement"),
+      ctx: document.querySelector("#canvasElement").getContext("2d"),
+    })
+    this.setupCamera();
   }
 
   runHandPose = async () => {
-    const net = await handpose.load()
-    console.log('Handpose model loaded')
-    
-    setInterval(() => {
-        this.detect(net)
-    }, 25);
+      const net = await handpose.load()
+      console.log('Handpose model loaded')
+      console.log(this.state.video)
+      setInterval(() => {
+          this.detect(net)
+      }, 25);
   }
 
   detect = async (net) => {
-    if (typeof this.state.webcamRef.current !== "undefined" && 
-        this.state.webcamRef.current !== null &&
-        this.state.webcamRef.current.video.readyState === 4) {
-
-        const hand = await net.estimateHands(this.state.webcamRef.current.video);
-        this.drawHand(hand);
-    }
+      const hand = await net.estimateHands(this.state.video);
+      this.state.ctx.clearRect(0, 0, this.state.canvas.width, this.state.canvas.height);
+      this.drawHand(hand);
 
   }
 
@@ -47,13 +48,46 @@ class App extends Component {
                   const x = landmarks[i][0]
                   const y = landmarks[i][1]
 
-                  const ctx = this.state.canvasRef.current.getContext("2d");
-                  ctx.beginPath();
-                  ctx.arc(x, y, 5, 0, 3 * Math.PI)
-                  ctx.fillStyle = "indigo"
-                  ctx.fill();
+                  this.state.ctx.beginPath();
+                  this.state.ctx.arc(x, y, 5, 0, 3 * Math.PI)
+                  let ctxElement = this.state.ctx
+                  ctxElement.fillStyle = "indigo"
+                  this.setState({ctx: ctxElement})
+                  this.state.ctx.fill();
               }
           })
+      }
+  }
+
+  setupWidthAndHeight = () => {
+      let videoElement = this.state.video
+      videoElement.width = this.state.videoConfig.width;
+      videoElement.height = this.state.videoConfig.height;
+
+      let canvasElement = this.state.canvas
+      canvasElement.width = this.state.videoConfig.width;
+      canvasElement.height = this.state.videoConfig.height;
+
+      this.setState({
+        video: videoElement,
+        canvas: canvasElement,
+      })
+  }
+
+  setupCamera = async () => {
+      if (navigator.mediaDevices.getUserMedia) {
+          navigator.mediaDevices.getUserMedia({ video: this.state.videoConfig })
+              .then((stream) => {
+                  let videoElement = this.state.video
+                  videoElement.srcObject = stream
+                  this.setState({video: videoElement})
+                  this.setupWidthAndHeight();
+                  this.runHandPose()
+              })
+              .catch(function (error) {
+                console.log(error)
+                  console.log("Something went wrong!");
+              });
       }
   }
 
@@ -61,17 +95,8 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-        <Webcam ref={this.state.webcamRef}
-        style={{
-          width: this.state.videoConfig.width,
-          height: this.state.videoConfig.height,
-        }}></Webcam>
-        <canvas ref={this.state.canvasRef}
-        style={{
-          position: "absolute",
-          width: this.state.videoConfig.width,
-          height: this.state.videoConfig.height,
-        }}></canvas>
+          <canvas id="canvasElement" style={{position: "absolute"}}></canvas>
+          <video autoPlay={true} id="videoElement"></video>    
         </header>
       </div>
     );
